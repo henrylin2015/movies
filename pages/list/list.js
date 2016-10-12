8c5d0bf3db60d3c2c91de3cbbc69f7d6634d257d
hailinglin <hailing.lin@modularrnd.com>
2016-10-12 17:31:13 +0800

    显示列表数据和编写了一个查询的方法使用

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,12 +1,50 @@
 // list.js
+// Douban API 操作
+const douban = require('../../utils/douban.js')
 Page({
     data:{
 	type:"正在上映",
-	title:"正在上映的电影-北京",
-	img: "https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2380677316.jpg"
+	title:"",
+	page: 1,
+	size: 20,
+	subtitle:'加载中...',
+	city:"上海",
+	loading:true,
+	hasMore: true,
+	movies: []
     },
-    onLoad () {
-	console.log("list")
+    loadMore(){
+	console.log("hasMore...")
+	if(!that.data.hasMore) return
+	that.setData({title:'加载中...',loading:true})
+	douban.find(that.data.type, that.data.page++,that.data.size)
+	    .then(d =>{
+		if(d.subjects.length){
+		    that.setData({title:d.title,movies:d.subjects,loading:false})
+		}else{
+		    that.setData({hasMore:false,loading:false})
+		}
+	    })
+	    .catch(e => {
+		that.setData({subtitle:'获取数据异常',movies:[],loading:false})
+		console.error(e)
+	    })
+    },
+    onLoad (params) {
+	var that = this
+	that.data.type = params.type || that.data.type
+	douban.find(that.data.type, that.data.page++,that.data.size)
+	    .then(d =>{
+		if(d.subjects.length){
+		    that.setData({title:d.title,movies:d.subjects,loading:false})
+		}else{
+		    that.setData({hasMore:false,loading:false})
+		}
+	    })
+	    .catch(e => {
+		that.setData({subtitle:'获取数据异常',movies:[],loading:false})
+		console.error(e)
+	    })
     },
     // 页面准备完成
     onReady() {
